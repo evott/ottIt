23dc0a29ad90d9b20b63b3107d6c4dcedb6f4477
evott <sheeph001@gmail.com>
2024-05-23 23:07:13 +0900

    초기 설정 끝

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, {useEffect} from "react";
+
+const Home = () => {
+    useEffect(() => {
+    }, []);
+
+    return (
+        <div className="container mx-auto p-6 absolute top-0">
+            <p className="text-3xl font-medium my-8">메인</p>
+        </div>
+    );
+};
+export default Home;
