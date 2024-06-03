39ff305c820d1b8d9e204cecdee8399410611b9d
heeseokid <heeseokid@g.shingu.ac.kr>
2024-06-03 11:26:25 +0900

    BookDetail 페이지 구성

--- frontend/src/pages/BookDetail.jsx
+++ frontend/src/pages/BookDetail.jsx
@@ -21,8 +21,8 @@ const BookDetail = () => {
                 <div className="flex w-72 rounded-md outline outline-1 outline-gray-200">
                     <div className="flex flex-col h-full p-2 m-2 w-1/2 items-center justify-center">
                         <div className="h-full w-auto flex flex-row items-center justify-center"> {/* 리뷰 토탈 상자 */}
-                            <p className="flex text-left font-semibold whitespace-pre items-center justify-center text-black text-xl">
-                                <img className="size-[2.3rem]" src="https://ifh.cc/g/D6VjnK.png"></img>10.0
+                            <p className="flex text-left font-semibold text-yellow-400 whitespace-pre items-center justify-center text-xl">
+                                <img className="size-[2.2rem] " src="https://ifh.cc/g/D6VjnK.png"></img>9.5
                             </p>
                         </div>
                         <div className="h-full w-auto flex flex-row items-center justify-center"> {/* 리뷰 상자 */}
@@ -37,8 +37,9 @@ const BookDetail = () => {
                         </div>
                     </div>
                     <div className="flex flex-col h-full p-2 m-2 w-1/2 items-center justify-center">
-                        <div className="h-full p-[0.45rem] w-auto flex flex-row items-center justify-center"> {/* 리뷰 토탈 상자 */}
-                                <img className="size-[1.3rem]" src="https://ifh.cc/g/XwKgY0.png"></img>
+                        <div
+                            className="h-full p-[0.45rem] w-auto flex flex-row items-center justify-center"> {/* 리뷰 토탈 상자 */}
+                            <img className="size-[1.3rem]" src="https://ifh.cc/g/XwKgY0.png"></img>
                         </div>
                         <div className="h-full w-auto flex flex-row items-center justify-center"> {/* 리뷰 상자 */}
                             <p className="flex text-left font-black whitespace-pre items-center justify-center text-indigo-900 text-[0.93rem]">도움돼요</p>
@@ -49,9 +50,8 @@ const BookDetail = () => {
                         </div>
                     </div>
                 </div>
-
-
             </div>
+
             <div className="flex-col flex items-center m-10 h-full w-full md:w-1/3 "> {/* 가운데 박스 */}
                 <p className={"text-2xl font-semibold"}>기분이 태도가 되지 말자</p>
                 <div className="join p-5"> {/* 책 종류 선정 */}
@@ -69,9 +69,59 @@ const BookDetail = () => {
                 </div>
             </div>
 
-            <div className="shadow-md flex-col w-full md:w-1/3"> {/* 오른쪽 박스 */}
-                <p>d</p>
+            <div className="flex-col flex h-full items-center md:w-1/3"> {/* 오른쪽 박스 */}
+                <div className="h-60"></div>
+                <div> {/* 책 출판 글 */}
+                    <div className="columns-3 w-[8.5rem] py-2 items-start justify-start"><p
+                        className="shadow text-xs text-center text-gray-500 font-medium rounded-sm outline-gray-200 outline outline-1 h-4 w-11">사은품</p>
+                        <p className="shadow text-xs text-gray-500 text-center font-medium rounded-sm outline-gray-200 outline outline-1 h-4 w-11">이벤트</p>
+                        <p className="shadow text-xs text-center text-gray-500 font-medium rounded-sm outline-gray-200 outline outline-1 h-4 w-14">소득공제</p>
+                    </div>
 
+                    <p className="text-xl mt-4 mb-4 text-left text-neutral-500 font-medium">
+                        <a className="text-2xl font-semibold text-yellow-400">10%</a>
+                        <a className="text-2xl ml-1.5 font-semibold text-black">14,400원</a>
+                        <a className="text-sm ml-1.5 line-through font-medium text-gray-600 ">16,000원</a>
+                    </p>
+                    <p className="border-y w-80 text-black border-solid flex py-5 text-left font-black mr-5 text-sm">적립/혜택
+                        <p className="items-center ml-[12.8rem] justify-center text-sm text-yellow-400">800P</p></p>
+                </div>
+                <div className="flex-col border-b border-solid h-[6.4rem] mr-4 flex w-80">
+                    <p className="mt-4 text-sm font-black text-black">배송안내
+                        <a className="font-black ml-[2.7rem] text-[0.8rem] text-black">도서 포함 15,000원 이상 무료배송</a>
+                    </p>
+                    <p className="ml-[7.6rem] text-sm font-black py-[0.3rem] text-black">
+                        <a className="text-[0.7rem] mr-2 text-indigo-600 px-[0.4rem] bg-gray-100 rounded-xl outline outline-1 outline-gray-200">당일배송</a>
+                        <a className="mt-[0.5rem] text-sm font-black text-black">오늘(6/3,월) 도착</a>
+                    </p>
+                    <p className="text-sm ml-[12.2rem] font-medium text-gray-500">
+                        기본배송지 기준
+                    </p>
+                </div>
+                <div className="flex-col border-b border-solid justify-center h-[4rem] mr-4 flex w-80">
+                    <p className="text-xs font-medium ml-[5rem] text-black">
+                        <a className="text-yellow-400 font-semibold">로그인 </a>
+                        후 <a className="text-xs font-semibold text-black">정확한 배송 안내</a>를 받아보세요!
+                    </p>
+                </div>
+                <div className="flex flex-row w-80 mt-4 justify-center"> {/* 하단부 */}
+                    <div className="w-1/1"> {/* 글 상자 */}
+                        <p className="text-xs font-semibold text-gray-500">알림 신청하시면 원하는 정보를 받아보실 수 있습니다.</p>
+                    </div>
+                    <div className="w-1/2 h-full ml-[3rem] flex flex-row "> {/* 알림 박스 */}
+                        <div
+                            className="flex flex-row items-center justify-center hover:bg-gray-200 cursor-pointer w-[5rem] h-[1.7rem] rounded outline outline-1 outline-indigo-800">
+                            <img className="size-4" src="https://ifh.cc/g/NwxjSc.png"/>
+                            <p className="ml-[0.1rem] text-xs text-indigo-800 font-semibold">알림신청</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-row w-80 mt-5 justify-center">
+                    <div className="flex flex-row items-center justify-center hover:bg-gray-200 cursor-pointer w-80 h-[2.5rem] rounded outline outline-1 outline-gray-300">
+                        <img className="flex items-center justify-center w-6 h-5" src="https://ifh.cc/g/Zcdvz2.png"/>
+                        <p className="text-[0.8rem] text-gray-900 font-semibold">매장 재고·위치</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
