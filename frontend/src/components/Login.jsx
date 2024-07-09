b9fcbcf00e3f4b83d936bff3e5795db5b2f65b2a
Chaeyeon1146 <codus04@g.shingu.ac.kr>
2024-07-09 15:37:31 +0900

    로그인 페이지 (ui 변경)

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,14 +1,15 @@
+import React, { useState } from 'react';
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const isFormFilled = email && password;
 
     return (
         <>
-
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-
-
-                </div>
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm"></div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form className="space-y-6" action="#" method="POST">
@@ -23,6 +24,8 @@ function Login() {
                                     type="email"
                                     autoComplete="email"
                                     required
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
@@ -34,7 +37,7 @@ function Login() {
                                     비밀번호를 입력해 주세요.
                                 </label>
                                 <div className="text-sm">
-                                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                    <a href="#" className="font-semibold text-gray-900 ">
                                         비밀번호를 잊어 버리셨나요?
                                     </a>
                                 </div>
@@ -46,15 +49,17 @@ function Login() {
                                     type="password"
                                     autoComplete="current-password"
                                     required
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
                         <div className="text-sm flex space-x-2 divide-x-[2px] divide-gray-400">
-                            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                            <a href="#" className="font-semibold text-gray-500">
                                 아이디 찾기
                             </a>
-                            <a href="#" className=" font-semibold text-indigo-600 hover:text-indigo-500 ">
+                            <a href="#" className="pl-2 font-semibold text-gray-500">
                                 비밀번호 찾기
                             </a>
                         </div>
@@ -62,22 +67,28 @@ function Login() {
                         <div>
                             <button
                                 type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className={`flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm ${
+                                    isFormFilled ? 'bg-blue-600 focus-visible:outline-blue-600' : 'bg-gray-400 focus-visible:outline-indigo-600'
+                                }`}
                             >
                                 로그인 하기
                             </button>
                         </div>
                     </form>
 
-                    <p className="mt-10 text-center text-sm text-gray-500">
-
-                        <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                        회원가입 하기
-                        </a>
-                    </p>
+                    <div className="mt-4">
+                        <button
+                            type="button"
+                            className="flex w-full justify-center rounded-md bg-white px-3 py-1.5 text-sm font-semibold leading-6 text-blue-800 focus:ring-2 focus:ring-indigo-600 focus:outline-none focus:ring-opacity-50"
+                        >
+                            회원가입 하기
+                        </button>
+                    </div>
                 </div>
             </div>
         </>
-    )
+    );
 }
+
 export default Login;
+
